7b6df008bb55df4f361171c2788dc0db91022fd8
ivan <ivan@ballab.com>
2014-07-02 17:37:15 +0200

    search div

--- main.php
+++ main.php
@@ -55,6 +55,7 @@ function layout($page_id) {
         <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 
         <script type="text/javascript">
+
             $(function() {
                 $( ".datepicker" ).datepicker({ dateFormat: "dd.mm.yy."/*"yy-mm-dd"*/, changeMonth: true, changeYear: true, yearRange:'-15:+15' }).val()
             });
@@ -74,24 +75,20 @@ function layout($page_id) {
                 $("#content").css('max-height',height);
                 $("#sidebar").css('height',height);
             });
+
+            $(document).ready(function() {
+                $("#id_search_btn").click(function() {
+                    $("#search").slideToggle();
+                    $("#sidebar_1").slideToggle();
+                });
+            });
+
         </script>
     </head>
     <body>
         <div id="container" style="position:relative;width:1100px;margin: 20px auto 0;overflow:hidden;">
             <div id="header" style="background-color:#D8D8D8;line-height:12px;margin:0 auto;padding:6px 12px;text-align: left;">
-                <?php
-                    include('includes/mysql_connect.php');
-                    $sql=mysql_query("SELECT id, naziv FROM partneri");
-                    //query
-                    if(mysql_num_rows($sql)) {
-                        $select= '<select id="partner_id" name="partner_id" class="dropdown" style="height:20px;">';
-                        while( $rs=mysql_fetch_array($sql)) {
-                            $select.='<option value='.$rs['id'].'>'.$rs['naziv'].'</option>';
-                        }
-                    }
-                    $select.='</select>';
-                    echo $select;
-                ?>
+                <button id="id_search_btn" class="btn btn-xs btn-default">Pretraga</button>
             </div>
             <div id="sidebar" style="background-color:#FFFFFF;overflow:auto;width:150px;float:left;text-align:left;padding:12px 12px; border-left:1px solid #D8D8D8;">
                 <a href="main.php" class="btn btn-xs btn-default">Go to Home page</a><br />
@@ -103,7 +100,44 @@ function layout($page_id) {
                 <a href="main.php?page=partners" class="btn btn-xs">Partneri</a><br />
                 <a href="main.php?page=racuni" class="btn btn-xs">Računi</a><br />
             </div>
+
+            <div id="search" style="background-color:#D8D8D8;overflow:auto;width:950px;padding:12px 12px;border-left:1px solid #D8D8D8;border-right:1px solid #D8D8D8;display: none;">
+                <?php
+                include('includes/mysql_connect.php');
+                //query
+                $sql=mysql_query("SELECT id, naziv FROM partneri");
+
+                if(mysql_num_rows($sql)) {
+                    $select= '<select id="id_partner_search" name="partner_search" onclick="is_selected(this);" class="select_big" autofocus="1" style="width:150px; height: 22px; font-size:12px;">';
+                    while( $rs=mysql_fetch_array($sql)) {
+                        $select.='<option value='.$rs['id'].'>'.$rs['naziv'].'</option>';
+                    }
+                }
+                $select.='</select>';
+                echo $select;
+                ?>
+                <?php
+                include('includes/mysql_connect.php');
+                $sql=mysql_query("SELECT id, status FROM statusi");
+                //query
+                if(mysql_num_rows($sql)) {
+                    $select= '<select id="id_status_search" name="status_search" class="select_big" style="width:180px; height: 22px; font-size:12px;">';
+                    while( $rs=mysql_fetch_array($sql)) {
+                        $select.='<option value='.$rs['id'].'>'.$rs['status'].'</option>';
+                    }
+                }
+                $select.='</select>';
+                echo $select;
+                ?>
+                <label>Od:</label>
+                <input type="text"  id="id_dat_od" name="dat_od" class="input_big datepicker" autocomplete="off" style="width:80px;margin-left:0px;">
+                <label>do:</label>
+                <input type="text"  id="id_dat_do" name="dat_do" class="input_big datepicker" autocomplete="off" style="width:80px;margin-left:0px;">
+                <label>god.</label>
+                <button id="id_search_go" class="btn btn-xs btn-default">OK</button>
+            </div>
             <div id="content" style="background-color:#FFFFFF;overflow:auto;width:950px;padding:12px 12px;border-left:1px solid #D8D8D8;border-right:1px solid #D8D8D8;">
+
                 <?php
                 if(isset($_GET['page']))
                 {
